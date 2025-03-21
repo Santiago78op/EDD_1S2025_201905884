ff02052e54e5505ad4476ba79f2ae15ff71f5e5f
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2025-03-21 12:31:51 -0600

    Proyecto Finalizado

--- Fase_2/AutoGestPro/AutoGestPro/obj/Debug/net9.0/AutoGestPro.AssemblyInfo.cs
+++ Fase_2/AutoGestPro/AutoGestPro/obj/Debug/net9.0/AutoGestPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoGestPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8289a2795b29d04939dcf2cc43635de2b1bb0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf889e76392c145404609cc215b3de78603b83b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoGestPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoGestPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
